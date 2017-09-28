3c1f7366cb8894b541c1d700260873beb744d986
Jorge Gutierrez <jgutierrezt2@hotmail.com>
2017-09-28 11:24:23 -0300

    added apps as applications in settings

--- CholitoProject/CholitoProject/settings.py
+++ CholitoProject/CholitoProject/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'animals',
+    'complaint',
+    'municipality',
+    'users'
 ]
 
 MIDDLEWARE = [
