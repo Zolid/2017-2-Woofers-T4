e216b23a7fffb475aa22e19a2a13025ecf025ca3
Jorge Gutierrez <jgutierrezt2@hotmail.com>
2017-10-03 15:45:13 -0300

    Added Complaint and ComplaintImage Models to complaint apps

--- CholitoProject/complaint/models.py
+++ CholitoProject/complaint/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Complaint(models.Model):
+	COMPLAINT_OPTIONS = (
+	(1, "Maltrato"),
+	)
+
+	description = models.TextField(max_length=1000)
+	case = models.SmallIntegerField(choices = COMPLAINT_OPTIONS)
+
+
+
+class ComplaintImages(models.Model):
+	image = models.ImageField()
+	complaint = models.ForeignKey('Complaint',on_delete=models.CASCADE)
+
+
