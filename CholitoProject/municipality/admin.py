4ef8d029fa9c81bfaad50d99eea15761378f6298
Jorge Gutierrez <jgutierrezt2@hotmail.com>
2017-10-08 16:56:09 -0300

    municipality and municipality's user model

--- CholitoProject/municipality/admin.py
+++ CholitoProject/municipality/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from municipality.models import Municipality, MunicipalityUser
 # Register your models here.
+
+
+class MunicipalityAdmin(admin.ModelAdmin):
+    fields = ('password', 'first_name', 'last_name', 'username',)
+
+
+admin.site.register(Municipality)
+admin.site.register(MunicipalityUser, MunicipalityAdmin)
--- CholitoProject/municipality/models.py
+++ CholitoProject/municipality/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+
+
+class Municipality(models.Model):
+    name = models.TextField(max_length=200)
+    lat = models.DecimalField(max_digits=9, decimal_places=6)
+    lng = models.DecimalField(max_digits=9, decimal_places=6)
+    directions = models.TextField(max_length=200)
+
+
+class MunicipalityUser(User):
+    municipality = models.ForeignKey('Municipality')
