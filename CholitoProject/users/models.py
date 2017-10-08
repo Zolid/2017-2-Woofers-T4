db27df2b2ebd2bc1ad82cd172c9d009e5288ae3d
Juan-Pablo Silva <jpsilvapena@hotmail.com>
2017-10-07 21:29:00 -0300

    Add user login logic

--- CholitoProject/users/models.py
+++ CholitoProject/users/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+UserTypes = (
+    (0, 'admin'),
+    (1, 'natural')
+)
+
+
+class AnUser(models.Model):
+    user = models.OneToOneField(User)
+    username = models.CharField(max_length=200)
+    user_type = models.IntegerField(default=1, choices=UserTypes)
--- CholitoProject/users/urls.py
+++ CholitoProject/users/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from .views import *
 
 urlpatterns = [
-    url(r'^login', LogInView.as_view(), name='login')
+    url(r'^login', LogInView.as_view(), name='login'),
+    url(r'^auth', AuthView.as_view(), name='auth'),
 ]
