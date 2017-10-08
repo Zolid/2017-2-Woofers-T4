9ccd0357ed9da53908e4638f4e61333b2890ce30
Jorge Gutierrez <jgutierrezt2@hotmail.com>
2017-10-08 11:38:26 -0300

    animal model added

--- CholitoProject/animals/models.py
+++ CholitoProject/animals/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from complaint.models import Complaint
 
 # Create your models here.
+
+
+class AnimalImage(models.Model):
+
+	image = models.ImageField(upload_to='animals/')
+	complaint = models.ForeignKey('Animal', on_delete=models.CASCADE)
+
+
+class Animal(models.Model):
+
+	GENDER_OPTIONS = (
+		("Male", 1),
+		("Female", 2),
+		)
+
+	name = models.TextField(max_length=100)
+	from_complaint = models.ForeignKey(Complaint, null=True)
+	gender = models.SmallIntegerField(choices=GENDER_OPTIONS)
+	description = models.TextField(max_length=1000)
