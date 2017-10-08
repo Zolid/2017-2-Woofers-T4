40f033bfd4ce1eba35fb05d7423e0a8af7dcc361
Jorge Gutierrez <jgutierrezt2@hotmail.com>
2017-10-08 11:42:08 -0300

    fixing settings static ROOT and dir to search accordin to https://docs.djangoproject.com/en/1.11/howto/static-files/deployment/

--- CholitoProject/CholitoProject/settings.py
+++ CholitoProject/CholitoProject/settings.py
@@ -121,8 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'statics/')
+# STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
