728232f90fde93119c3ca38696d92a484e43939e
Jorge Gutierrez <jgutierrezt2@hotmail.com>
2017-10-08 18:34:49 -0300

    fixed first coments

--- CholitoProject/animals/models.py
+++ CholitoProject/animals/models.py
@@ -1,23 +1,25 @@
 from django.db import models
-from complaint.models import Complaint
-
-# Create your models here.
+from complaint.models import Complaint, AnimalType
 
 
 class AnimalImage(models.Model):
 
-	image = models.ImageField(upload_to='animals/')
-	complaint = models.ForeignKey('Animal', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='animals/')
+    complaint = models.ForeignKey('Animal', on_delete=models.CASCADE)
 
 
 class Animal(models.Model):
 
-	GENDER_OPTIONS = (
-		("Male", 1),
-		("Female", 2),
-		)
+    GENDER_OPTIONS = (
+        ("Male", 1),
+        ("Female", 2),
+    )
 
-	name = models.TextField(max_length=100)
-	from_complaint = models.ForeignKey(Complaint, null=True)
-	gender = models.SmallIntegerField(choices=GENDER_OPTIONS)
-	description = models.TextField(max_length=1000)
+    name = models.TextField(max_length=100)
+    from_complaint = models.ForeignKey(Complaint, null=True)
+    gender = models.SmallIntegerField(choices=GENDER_OPTIONS)
+    description = models.TextField(max_length=1000)
+    animal_type = models.ForeignKey(AnimalType)
+    gender = models.SmallIntegerField(choices=GENDER_OPTIONS)
+    color = models.TextField(max_length=50)
+    wounded = models.BooleanField()
--- CholitoProject/complaint/models.py
+++ CholitoProject/complaint/models.py
@@ -1,9 +1,31 @@
 from django.db import models
 
 
+class AnimalType(models.Model):
+    name = models.TextField(max_length=100)
+
+
 class Complaint(models.Model):
+
     COMPLAINT_OPTIONS = (
-        (1, "Maltrato"),
+        (1, "Abandono en la calle"),
+        (2, "Exposición a temperaturas extremas"),
+        (3, "Falta de agua"),
+        (4, "Falta de comida"),
+        (5, "Violencia"),
+        (6, "Venta ambulante"),
+    )
+
+    COMPLAINT_STATUS = (
+        (1, "Enviado"),
+        (2, "En Gestión"),
+        (2, "Resuleta"),
+        (2, "Erronea"),
+    )
+
+    GENDER_OPTIONS = (
+        ("Male", 1),
+        ("Female", 2),
     )
 
     description = models.TextField(max_length=1000)
@@ -11,6 +33,9 @@ class Complaint(models.Model):
     lat = models.DecimalField(max_digits=9, decimal_places=6, null=True)
     lng = models.DecimalField(max_digits=9, decimal_places=6, null=True)
     directions = models.TextField(max_length=200, null=True)
+    status = models.SmallIntegerField(choices=COMPLAINT_STATUS)
+    animal_type = models.ForeignKey(AnimalType)
+    gender = models.SmallIntegerField(choices=GENDER_OPTIONS)
 
 
 class ComplaintImages(models.Model):
