4b9dbb6eb016fe9f0b6219fbc3ab48f3b3fd0a47
Juan-Pablo Silva <jpsilvapena@hotmail.com>
2017-10-21 20:34:03 -0300

    Fix user and animals not displaying

--- CholitoProject/animals/views.py
+++ CholitoProject/animals/views.py
@@ -1,29 +1,24 @@
-from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 
+from CholitoProject.userManager import get_user_index
 from animals.models import Animal, Adopt
+from complaint.models import AnimalType
 from naturalUser.models import NaturalUser
-
-
-# TODO: ONG can see this pago too
-def is_adopt_selected(request, adopt_users):
-    try:
-        natural_user = NaturalUser.objects.get(user=request.user)
-    except ObjectDoesNotExist:
-        return False
-    return natural_user in adopt_users
 
 
 class AnimalRenderView(View):
     template_name = 'view_animal.html'
-    context = {}
+    context = {'animals': AnimalType.objects.all()}
 
     def get(self, request, pk, **kwargs):
+        c_user = get_user_index(request.user)
+        self.context['c_user'] = c_user
+
         animal = get_object_or_404(Animal, pk=pk)
         adopt_users_pk = Adopt.objects.filter(animal=animal).values('user')
         adopt_users = list(NaturalUser.objects.filter(pk__in=adopt_users_pk))
-        self.context['animal'] = animal
+        self.context['selected_animal'] = animal
         self.context['adopters'] = adopt_users
-        self.context['adopt_selected'] = is_adopt_selected(request, adopt_users)
+        self.context['adopt_selected'] = c_user in adopt_users
         return render(request, self.template_name, context=self.context)
