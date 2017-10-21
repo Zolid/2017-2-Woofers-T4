41448b67176987e43282140623cdf05710efbac5
Jorge Gutierrez <jgutierrezt2@hotmail.com>
2017-10-21 20:25:58 -0300

    added elements to redirect  to same url and different user types, also is auth implemented

--- CholitoProject/municipality/urls.py
+++ CholitoProject/municipality/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import url
-from django.contrib.auth.decorators import login_required
 
 from municipality.views import IndexView
 
 urlpatterns = [
-    url(r'^(?P<pk>\d+)/$', login_required(IndexView.as_view()), name='municipality-index'),
+    url(r'^$', IndexView.as_view(), name='municipality-index'),
 ]
