3202c093082ce71ff9a17cf4d93c5ccdca7f1979
Juan-Pablo Silva <jpsilvapena@hotmail.com>
2017-10-22 16:06:15 -0300

    Municipality template in complaint view

--- CholitoProject/complaint/views.py
+++ CholitoProject/complaint/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 
+from CholitoProject.userManager import get_user_index
 from complaint.forms import ComplaintForm
-from complaint.models import Complaint
+from complaint.models import Complaint, ComplaintImage
 
 
 class ComplaintView(View):
@@ -14,7 +16,9 @@ class ComplaintView(View):
         return render(request, self.template_name, context=self.context)
 
 
-class ComplaintSendView(View):
+class ComplaintSendView(PermissionRequiredMixin, LoginRequiredMixin, View):
+    permission_required = 'naturalUser.natural_user_access'
+
     def post(self, request, **kwargs):
         form = ComplaintForm(request.POST)
         if form.is_valid():
@@ -30,6 +34,12 @@ class ComplaintRenderView(View):
     context = {}
 
     def get(self, request, pk, **kwargs):
+        user = get_user_index(request.user)
+        self.context['c_user'] = user
         complaint = get_object_or_404(Complaint, pk=pk)
         self.context['complaint'] = complaint
+
+        images = ComplaintImage.objects.filter(complaint=complaint)
+        self.context['images'] = images
+
         return render(request, self.template_name, context=self.context)
