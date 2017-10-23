2d3cf7850a53c04b71fe92925a5b141ed72a100b
Juan-Pablo Silva <jpsilvapena@hotmail.com>
2017-10-23 18:33:28 -0300

    Fix bug in complaint update

--- CholitoProject/complaint/views.py
+++ CholitoProject/complaint/views.py
@@ -68,7 +68,7 @@ class ComplaintActState(PermissionRequiredMixin, LoginRequiredMixin, View):
         images = ComplaintImage.objects.filter(complaint=complaint)
         self.context['images'] = images
 
-        return render(request, self.template_name, context=self.context)
+        return redirect('see-complaint', pk=pk)#render(request, self.template_name, context=self.context)
 
 
 
