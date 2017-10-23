60b3793cab9522404a7e4d1c2efa3d71d5478f47
Jorge Gutierrez <jgutierrezt2@hotmail.com>
2017-10-23 01:02:59 -0300

    added municipality edit avatar

--- CholitoProject/municipality/urls.py
+++ CholitoProject/municipality/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from municipality.views import IndexView
+from municipality.views import IndexView, UserDetail
 
 urlpatterns = [
+    url(r'^user/$', UserDetail.as_view()),
     url(r'^$', IndexView.as_view(), name='municipality-index'),
 ]
